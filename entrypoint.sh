3f5d8aa8e66e77525f05d7d8d04a3e239d3bb8b7
iromli <isman.firmansyah@gmail.com>
2018-08-28 01:11:30 +0700

    add support for RHEL

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 if [ ! -f /touched ]; then
-    python /opt/scripts/entrypoint.py
+    if [ -f /etc/redhat-release ]; then
+        source scl_source enable python27 && python /opt/scripts/entrypoint.py
+    else
+        python /opt/scripts/entrypoint.py
+    fi
     touch /touched
 fi
 
