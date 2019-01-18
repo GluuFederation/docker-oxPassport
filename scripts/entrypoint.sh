9c22d2922e69a82064778696c4edcf39101665d9
iromli <isman.firmansyah@gmail.com>
2019-01-19 02:39:47 +0700

    fix call to wait_for script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
+set -e
+
+if [ -f /etc/redhat-release ]; then
+    source scl_source enable python27 && python /opt/scripts/wait_for.py --deps="config,secret"
+else
+    python /opt/scripts/wait_for.py --deps="config,secret"
+fi
+
 if [ ! -f /deploy/touched ]; then
     if [ -f /touched ]; then
         mv /touched /deploy/touched
     else
         if [ -f /etc/redhat-release ]; then
-            source scl_source enable python27 && python /opt/scripts/wait_for.py --deps="config,secret" && python /opt/scripts/entrypoint.py
+            source scl_source enable python27 && python /opt/scripts/entrypoint.py
         else
-            python /opt/scripts/wait_for.py --deps="config,secret" && python /opt/scripts/entrypoint.py
+            python /opt/scripts/entrypoint.py
         fi
 
         touch /deploy/touched
